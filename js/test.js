8bff2d1b10f5de83e2f9bf44c552f715542fff3d
Shosuke Doi <doiverson.canada@gmail.com>
2018-10-29 14:04:11 -0700

    fix some modules

---
+++ js/test.js
@@ -0,0 +1,6 @@
+
+import {init} from "../node_modules/ityped/dist/index";
+
+init('.ityped', {
+    strings:['Dead simple animated typed.', 'No jQuery dependency.', 'Tiny size: <3kb.', 'Just install and Enjoy!']
+});
