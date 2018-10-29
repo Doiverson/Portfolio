b839862e1d51134780ccbde5308845fffb3132b8
Shosuke Doi <doiverson.canada@gmail.com>
2018-10-29 15:55:49 -0700

    added typewriter effect and About Me section

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+// Type Writer
+const iTyped = document.querySelector('.ityped');
+
+window.ityped.init(iTyped, {
+    strings: ['Dead simple animated typing.', 'No dependencies'],
+    typeSpeed: 55,
+    backSpeed: 30,
+    startDelay: 500,
+    backDelay: 500,
+    loop: true,
+    showCursor: true,
+    cursorChar: "|",
+    onFinished: function(){}
+});
--- js/test.js
+++
@@ -1,6 +0,0 @@
-
-import {init} from "../node_modules/ityped/dist/index";
-
-init('.ityped', {
-    strings:['Dead simple animated typed.', 'No jQuery dependency.', 'Tiny size: <3kb.', 'Just install and Enjoy!']
-});
