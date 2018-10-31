10559dbb87ba1ff0d2d775051087e0c35c6ad2aa
Shosuke Doi <doiverson.canada@gmail.com>
2018-10-31 01:45:59 -0700

    added slide-in effect, almost done on mobile layout

---
+++ js/_slidein.js
@@ -0,0 +1,38 @@
+
+function debounce(func, wait = 10, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+console.log(sliderImages);
+
+function checkSlide() {
+
+    sliderImages.forEach(sliderImage => {
+        // half way through the image
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.clientHeight / 2;
+        // bottom of the image
+        const imageBottom = sliderImage.offsetTop + sliderImage.clientHeight;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
