0c6897ae694863f31d11bdffc4d9457124b39615
Shosuke Doi <doiverson.canada@gmail.com>
2018-12-08 16:56:46 -0800

    from seattle

--- js/toggle.js
+++ js/toggle.js
@@ -1,3 +1,17 @@
+
+//Mobile nav bar toggle
 $('#nav-toggle').click(function(){
     $('body').toggleClass('open');
 });
+
+
+//Close nav bar after link is clicked
+const open = document.body;
+const btn = document.getElementById('global-nav').getElementsByTagName('a');
+console.log(open);
+
+for(let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', () => {
+       open.classList.remove('open');
+    },false);
+}
