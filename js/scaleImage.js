6278580c76a301eab60f2df05f32f35211e1a8a2
Shosuke Doi <doiverson.canada@gmail.com>
2019-05-25 14:58:21 -0700

    fixed resume button and scale img effect

--- js/scaleImage.js
+++ js/scaleImage.js
@@ -1,12 +1,16 @@
-
-const link = document.querySelectorAll(".work-ttl ")
+const link = document.querySelectorAll(".work-ttl")
 const linkArray = Array.from(link);
+
+const link2 = document.querySelectorAll(".work-ttl-2")
+const linkArray2 = Array.from(link2);
 
 const img = document.querySelectorAll(".work img")
 const imgArray = Array.from(img);
 
 
+
 const scaleImg = (index) => {
+    console.log("mouseover");
     imgArray.map((img, i) => {
         if(index === i){
             img.classList.add("scale");
@@ -26,6 +30,15 @@ const defaultImg = (index) => {
 linkArray.map((a) => {
 
     let imgIndex = linkArray.indexOf(a);
+
+    a.addEventListener("mouseover", scaleImg.bind(null, imgIndex) );
+
+    a.addEventListener("mouseout", defaultImg.bind(null, imgIndex) );
+})
+
+linkArray2.map((a) => {
+
+    let imgIndex = linkArray2.indexOf(a);
 
     a.addEventListener("mouseover", scaleImg.bind(null, imgIndex) );
 
